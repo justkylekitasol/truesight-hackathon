2dd24ebacbce7a101221ba7e103b5f7992d6554f
Dexter Turno <dbturno@gmail.com>
2022-02-16 11:17:31 +0800

    Spring Boot Backend service initial commit

---
+++ truesight-backend/src/main/java/com/hackatoon/truesightbackend/TruesightBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hackatoon.truesightbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruesightBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruesightBackendApplication.class, args);
+	}
+
+}
---
+++ truesight-backend/src/test/java/com/hackatoon/truesightbackend/TruesightBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackatoon.truesightbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruesightBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
